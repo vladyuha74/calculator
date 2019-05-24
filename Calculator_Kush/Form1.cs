b9d8d68674ecf562403fc01608cd3afb50697c0d
vladyuha74 <xXx.vladyuha74.xXx@gmail.ru>
2019-05-24 14:21:31 +0500

    Калькулятор обнова

--- Calculator_Kush/Form1.cs
+++ Calculator_Kush/Form1.cs
@@ -74,15 +74,138 @@ namespace Calculator_Kush
             {
                 stat = true;
                 textBox1.Text = "";
-                result = "/";
+                result = "*";
             }
             if (nb == 14)
             {
                 stat = true;
                 textBox1.Text = "";
-                result = "*";
+                result = "/";
             }
         }
+
+
+        public void Chislo(int nb)
+        {
+            if(stat == false)
+            {
+                if (nb == 1) // Проверка нажатия конкретной кнопки 
+                {
+                    s_ch1 = s_ch1 + Convert.ToString(nb);
+                    textBox1.Text = s_ch1;
+                }
+                if (nb == 2) // Проверка нажатия конкретной кнопки 
+                {
+                    s_ch1 = s_ch1 + Convert.ToString(nb);
+                    textBox1.Text = s_ch1;
+                }
+                if (nb == 3)
+                {
+                    s_ch1 = s_ch1 + Convert.ToString(nb);
+                    textBox1.Text = s_ch1;
+                }
+                if (nb == 4) // Проверка нажатия конкретной кнопки 
+                {
+                    s_ch1 = s_ch1 + Convert.ToString(nb);
+                    textBox1.Text = s_ch1;
+                }
+                if (nb == 5)
+                {
+                    s_ch1 = s_ch1 + Convert.ToString(nb);
+                    textBox1.Text = s_ch1;
+                }
+                if (nb == 6) // Проверка нажатия конкретной кнопки 
+                {
+                    s_ch1 = s_ch1 + Convert.ToString(nb);
+                    textBox1.Text = s_ch1;
+                }
+                if (nb == 7)
+                {
+                    s_ch1 = s_ch1 + Convert.ToString(nb);
+                    textBox1.Text = s_ch1;
+                }
+                if (nb == 8) // Проверка нажатия конкретной кнопки 
+                {
+                    s_ch1 = s_ch1 + Convert.ToString(nb);
+                    textBox1.Text = s_ch1;
+                }
+                if (nb == 9)
+                {
+                    s_ch1 = s_ch1 + Convert.ToString(nb);
+                    textBox1.Text = s_ch1;
+                }
+                if (nb == 0) // Проверка нажатия конкретной кнопки 
+                {
+                    s_ch1 = s_ch1 + Convert.ToString(nb);
+                    textBox1.Text = s_ch1;
+
+                }
+                if (nb == 16)
+                {
+                    s_ch1 = s_ch1 + ",";
+                    textBox1.Text = s_ch1;
+                }
+            }
+            else
+            {
+                if (nb == 1) // Проверка нажатия конкретной кнопки 
+                {
+                    s_ch2 = s_ch2 + Convert.ToString(nb);
+                    textBox1.Text = s_ch2;
+                }
+                if (nb == 2) // Проверка нажатия конкретной кнопки 
+                {
+                    s_ch2 = s_ch2 + Convert.ToString(nb);
+                    textBox1.Text = s_ch2;
+                }
+                if (nb == 3)
+                {
+                    s_ch2 = s_ch2 + Convert.ToString(nb);
+                    textBox1.Text = s_ch2;
+                }
+                if (nb == 4) // Проверка нажатия конкретной кнопки 
+                {
+                    s_ch2 = s_ch2 + Convert.ToString(nb);
+                    textBox1.Text = s_ch2;
+                }
+                if (nb == 5)
+                {
+                    s_ch2 = s_ch2 + Convert.ToString(nb);
+                    textBox1.Text = s_ch2;
+                }
+                if (nb == 6) // Проверка нажатия конкретной кнопки 
+                {
+                    s_ch2 = s_ch2 + Convert.ToString(nb);
+                    textBox1.Text = s_ch2;
+                }
+                if (nb == 7)
+                {
+                    s_ch2 = s_ch2 + Convert.ToString(nb);
+                    textBox1.Text = s_ch2;
+                }
+                if (nb == 8) // Проверка нажатия конкретной кнопки 
+                {
+                    s_ch2 = s_ch2 + Convert.ToString(nb);
+                    textBox1.Text = s_ch2;
+                }
+                if (nb == 9)
+                {
+                    s_ch2 = s_ch2 + Convert.ToString(nb);
+                    textBox1.Text = s_ch2;
+                }
+                if (nb == 0) // Проверка нажатия конкретной кнопки 
+                {
+                    s_ch2 = s_ch2 + Convert.ToString(nb);
+                    textBox1.Text = s_ch2;
+                }
+                if (nb == 16)
+                {
+                    s_ch2 = s_ch2 + ",";
+                    textBox1.Text = s_ch2;
+                }
+            }
+        }
+
 
         public void Infomation_btn_click1(int nb)
         {
@@ -208,140 +331,69 @@ namespace Calculator_Kush
             // Переменые кторые фиксирует нажата кнопка или нет.
             int numb = 1; // Номер кнопки
             Info(numb);
-            if (stat == false)
-            {
-                Infomation_btn_click1(numb);
-            }
-            if (stat == true)
-            {
-                Infomation_btn_click2(numb);
-            }
+            Chislo(numb);
+            //if (stat == false)
+           // {
+           //     Infomation_btn_click1(numb);
+          //  }
+            //if (stat == true)
+          //  {
+           //     Infomation_btn_click2(numb);
+           // }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             int numb = 2;// Номер кнопки
-            Info(numb);
-            if (stat == false)
-            {
-                Infomation_btn_click1(numb);
-            }
-            if (stat == true)
-            {
-                Infomation_btn_click2(numb);
-            }
+            Chislo(numb);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             int numb = 3; // Номер кнопки
-            Info(numb);
-            if (stat == false)
-            {
-                Infomation_btn_click1(numb);
-            }
-            if (stat == true)
-            {
-                Infomation_btn_click2(numb);
-            }
+            Chislo(numb);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             int numb = 4; // Номер кнопки
-            Info(numb);
-            if (stat == false)
-            {
-                Infomation_btn_click1(numb);
-            }
-            if (stat == true)
-            {
-                Infomation_btn_click2(numb);
-            }
+            Chislo(numb);
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
             int numb = 5; // Номер кнопки
-            Info(numb);
-            if (stat == false)
-            {
-                Infomation_btn_click1(numb);
-            }
-            if(stat == true)
-            {
-                Infomation_btn_click2(numb);
-            }
+            Chislo(numb);
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
             int numb = 6;
-            Info(numb);
-            if (stat == false)
-            {
-                Infomation_btn_click1(numb);
-            }
-            else
-            {
-                Infomation_btn_click2(numb);
-            }
+            Chislo(numb);
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
             int numb = 7; // Номер кнопки
-            Info(numb);
-            if (stat == false)
-            {
-                Infomation_btn_click1(numb);
-            }
-            if (stat == true)
-            {
-                Infomation_btn_click2(numb);
-            }
+            Chislo(numb);
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
             int numb = 8; // Номер кнопки
-            Info(numb);
-            if (stat == false)
-            {
-                Infomation_btn_click1(numb);
-            }
-            if (stat == true)
-            {
-                Infomation_btn_click2(numb);
-            }
+            Chislo(numb);
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
             int numb = 9;// Номер кнопки
-            Info(numb);
-            if (stat == false)
-            {
-                Infomation_btn_click1(numb);
-            }
-            if (stat == true)
-            {
-                Infomation_btn_click2(numb);
-            }
+            Chislo(numb);
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
             int numb = 0;// Номер кнопки
-            Info(numb);
-            if (stat == false)
-            {
-                Infomation_btn_click1(numb);
-            }
-            if (stat == true)
-            {
-                Infomation_btn_click2(numb);
-            }
+            Chislo(numb);
         }
 
         private void button11_Click(object sender, EventArgs e)
@@ -376,15 +428,7 @@ namespace Calculator_Kush
         private void button16_Click(object sender, EventArgs e)
         {
             int numb = 16;// Номер кнопки
-            Info(numb);
-            if (stat == false)
-            {
-                Infomation_btn_click1(numb);
-            }
-            if (stat == true)
-            {
-                Infomation_btn_click2(numb);
-            }
+            Chislo(numb);
         }
 
         private void button17_Click(object sender, EventArgs e)
@@ -392,6 +436,7 @@ namespace Calculator_Kush
             s_ch1 = "";
             s_ch2 = "";
             textBox1.Text = "";
+            stat = false;
         }
 
         private void button18_Click(object sender, EventArgs e)
